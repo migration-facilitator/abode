9858510fe28cd601cad7c335141c4ea558c5cec1
AndrewTrahan <globalART19@gmail.com>
2018-07-26 00:15:00 -0500

    landing page complete. terrible color scheme and no content...but its complete

--- client/app.js
+++ client/app.js
@@ -1,11 +1,12 @@
 import React from 'react'
 
-import {Navbar} from './components'
+import { Navbar } from './components'
 import Routes from './routes'
+import { colors } from './color-palette'
 
 const App = () => {
   return (
-    <div>
+    <div style={{ backgroundColor: colors.main }}>
       <Navbar />
       <Routes />
     </div>
--- client/components/landing-page.js
+++ client/components/landing-page.js
@@ -1,21 +1,80 @@
 import React from 'react'
-import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
+import styled from 'styled-components'
+import { colors } from '../color-palette'
 
-/**
- * COMPONENT
- */
+const Wrapper = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  flex-flow: column;
+  // justify-content: center;
+  align-items: center;
+  width: 80%;
+  height: calc(100vh - 6em - 10px);
+  max-width: 80em;
+  margin: 0 auto 0 auto;
+  padding: 1em;
+`
+const MainCard = styled.div`
+  position: relative;
+  width: 80%;
+  background-color: ${colors.light};
+  height: 20em;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin: 1em auto 0 auto;
+  padding: 1em;
+`
+const Title = styled.p`
+  color: ${colors.accent};
+  font-size: 5em;
+  font-weight: bold;
+  margin: 0;
+  text-shadow: 3px 2px ${colors.dark}55;
+`
+const ButtonDiv = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 1em auto 0 auto;
+`
+const Button = styled.button`
+  margin: 0 .5em 0 .5em;
+  border-radius: 1em;
+  background-color: ${colors.accent};
+  color: ${colors.dark};
+  border: 0;
+  padding: .8em 1em;
+  font-weight: bold;
+  font-size: 1em;
+  box-shadow: 3px 8px 16px 0 ${colors.dark}55;
+`
+// position: relative,
+
 export const LandingPage = () => {
   return (
-    <div>
-      <h3>Landing Page</h3>
-    </div>
+    <Wrapper>
+      <Title>
+        Abode
+      </Title>
+      <MainCard>
+        Making relocation easy!!!
+      </MainCard>
+      <ButtonDiv>
+        <Button type='button' >
+          Start Search
+          </Button>
+        <Button type='button' >
+          Sign Up
+          </Button>
+      </ButtonDiv>
+    </Wrapper>
   )
 }
 
-/**
- * CONTAINER
- */
 const mapState = state => {
   return {
     email: state.user.email
@@ -23,9 +82,3 @@ const mapState = state => {
 }
 
 export default connect(mapState)(LandingPage)
-
-/**
- * PROP TYPES
- */
-LandingPage.propTypes = {
-}
