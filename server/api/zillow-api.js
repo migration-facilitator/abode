e6d2d09c21afd9da3c8674eb71f702f363b57ffc
AndrewTrahan <globalART19@gmail.com>
2018-08-17 14:35:39 -0500

    convevrted zillow api output from xml to json

--- server/api/zillow-api.js
+++ server/api/zillow-api.js
@@ -1,13 +1,17 @@
 const router = require('express').Router()
 const axios = require('axios')
-const { zillowApiKey } = require('../../secrets')
+const convert = require('xml-js')
 module.exports = router
+
+const zillowApiKey = process.env.ZILLOW_API_KEY
 
 router.get('/neighborhood', async (req, res, next) => {
   try {
-    const data = await axios.get(`http://www.zillow.com/webservice/GetRegionChildren.htm?zws-id=${zillowApiKey}&state=il&city=chicago&childtype=neighborhood`)
+    console.log(zillowApiKey)
+    const { data } = await axios.get(`http://www.zillow.com/webservice/GetRegionChildren.htm?zws-id=${zillowApiKey}&state=il&city=chicago&childtype=neighborhood`)
     //data comes back as an xml doc
-    res.json(data)
+    const jsonData = convert.xml2js(data, { compact: true, spaces: 0 })
+    res.json(jsonData)
   } catch (err) {
     next(err)
   }
